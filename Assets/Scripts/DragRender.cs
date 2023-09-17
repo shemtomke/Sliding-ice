be7bc680ffcb0df52f50563d50adc313b0fc4863
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2023-09-17 03:39:03 +0300

    Update

---
+++ Assets/Scripts/DragRender.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragRender : MonoBehaviour
+{
+    public List<Sprite> topDragUINotifications;
+    public List<Sprite> dragDots;
+
+    public Vector2 startDragPos;
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,6 @@ public class Player : MonoBehaviour
     public float power = 10f;
     public float maxDrag = 5f;
     public Rigidbody2D rb;
-    public GameObject[] dragRender;
 
     Touch touch;
     Vector2 dragStartPos;
@@ -36,17 +35,41 @@ public class Player : MonoBehaviour
                 DragRelease();
             }
         }
+
+        // Check if the left mouse button is pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            DragStart();
+        }
+
+        // Check if the left mouse button is held down and moving
+        if (Input.GetMouseButton(0))
+        {
+            Dragging();
+        }
+
+        // Check if the left mouse button was released
+        if (Input.GetMouseButtonUp(0))
+        {
+            DragRelease();
+        }
     }
     void DragStart()
     {
-
+        dragStartPos = Camera.main.ScreenToWorldPoint(touch.position);
+        Debug.Log("Dragging Start!");
     }
     void Dragging()
     {
-
+        Debug.Log("Dragging!");
     }
     void DragRelease()
     {
+        Debug.Log("Dragging Released!");
+        Vector2 drafReleasePos = Camera.main.ScreenToWorldPoint(touch.position);
+        Vector2 force = dragStartPos - drafReleasePos;
+        Vector2 clampedForce = Vector2.ClampMagnitude(force, maxDrag) * power;
 
+        rb.AddForce(clampedForce, ForceMode2D.Impulse);
     }
 }
